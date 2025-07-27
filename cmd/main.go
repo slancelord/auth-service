f577ca97dd10665f8d3de6856033cb2e71aa1d91
Sergey <slancelord@gmail.com>
2025-07-27 05:25:41 +0500

    Add database init

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"auth-service/internal/config"
+	"auth-service/internal/database"
 	"auth-service/internal/server"
 )
 
 func main() {
-	config.InitConfig()
+	config.Init()
+
+	database.Init()
+	defer database.GetDB().Close()
+
 	server.Serve()
 }
