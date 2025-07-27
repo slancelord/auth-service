d61c2ff2d7395b2c83bb979111df96951f23fc8f
Sergey <slancelord@gmail.com>
2025-07-27 22:51:11 +0500

    Refactor TokenHandler

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -8,25 +8,44 @@ import (
 )
 
 // TokenHandler godoc
-// @Summary Получить токены
-// @Description Генерирует access и refresh токены по GUID
+// @Summary Get tokens
+// @Description Generates access and refresh tokens using a user GUID
 // @Tags auth
 // @Accept json
 // @Produce json
-// @Param guid query string true "GUID пользователя"
+// @Param User-Agent header string true "User Agent" Enums(Mozilla/5.0, Opera/9.80)
 // @Success 200 {object} map[string]string
-// @Failure 400 {string} string "Missing guid"
+// @Failure 400 {string} string "Missing GUID"
 // @Failure 500 {string} string "Failed to generate tokens"
-// @Router /auth/token [post]
+// @Router /api/auth/token [post]
 func TokenHandler(w http.ResponseWriter, r *http.Request) {
+	refreshExpireDay := time.Duration(config.GetConfig().RefreshExpireDay)
+	sessionRepo := repository.NewRepository[model.Session]()
+
 	guid := r.URL.Query().Get("guid")
 	if guid == "" {
 		http.Error(w, "Missing guid", http.StatusBadRequest)
 	}
 
-	accessToken, refreshToken, err := service.GenerateToken(guid)
+	accessToken, refreshToken, tokenPairId, err := service.GenerateToken(guid)
 	if err != nil {
 		http.Error(w, "Failed to generate tokens", http.StatusInternalServerError)
+		return
+	}
+
+	session := model.Session{
+		UserID:       guid,
+		RefreshToken: service.HashToken(refreshToken),
+		TokenPairId:  tokenPairId,
+		UserAgent:    r.UserAgent(),
+		IPAddress:    r.RemoteAddr,
+		CreatedAt:    time.Now(),
+		ExpiresAt:    time.Now().Add(refreshExpireDay * 24 * time.Hour),
+	}
+
+	err = sessionRepo.Create(&session)
+	if err != nil {
+		http.Error(w, "Failed to save session", http.StatusInternalServerError)
 		return
 	}
 
